cda9f37f66cb52a50ae073c5c2b81751fd1f2e7e
suengj <18134241+suengj@users.noreply.github.com>
2025-02-01 21:46:52 +0900

    Update llm_dict.py

--- utils/llm_dict.py
+++ utils/llm_dict.py
@@ -1,7 +1,8 @@
 LLM_DICT = {
     'Open AI': {
         'API': 'OPENAI_API_KEY',
-        'MODEL': ['o1-mini-2024-09-12',
+        'MODEL': ['o3-mini-2025-01-31',
+                  'o1-mini-2024-09-12',
                   'gpt-4o-mini-2024-07-18',
                   'gpt-3.5-turbo-0125'],
         'ETC': []
